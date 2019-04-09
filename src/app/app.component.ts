6b97c1509b1354cd5ab6572f6ee0c7e7f9c69673
wangrui-uk <androidoven@gmail.com>
2019-04-09 22:47:19 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'som-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'smart-office-monitor';
+}
